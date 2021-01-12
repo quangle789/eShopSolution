58b061793b643808bee65df859edfacdf9e7c6c8
quangle789 <quanglm72@wru.vn>
2021-01-12 23:43:21 +0700

    Thêm bảng thực thể

---
+++ eShopSolution.Data/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class AppUser : IdentityUser<Guid>
+    {
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+    }
+}
