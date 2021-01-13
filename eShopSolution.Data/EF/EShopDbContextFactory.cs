b3adeec76c30f04f7cd0d51c45c936bd8b80aee2
quangle789 <quanglm72@wru.vn>
2021-01-13 10:00:29 +0700

    tạo kết nối và tạo database

---
+++ eShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            //đọc chuỗi kết nối từ file json
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            var connectionString = configuration.GetConnectionString("eShopSolution");
+
+            var optionBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionBuilder.UseMySql(connectionString);
+
+            return new EShopDbContext(optionBuilder.Options);
+        }
+    }
+}
