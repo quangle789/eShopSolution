0133e9040780540d2468c52ca77bf84f75b319fa
quangle789 <quanglm72@wru.vn>
2021-01-13 10:35:42 +0700

    cập nhập database

---
+++ eShopSolution.Data/Migrations/20210113033418_Initial2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2021, 1, 13, 10, 34, 17, 963, DateTimeKind.Local).AddTicks(9219),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldDefaultValue: new DateTime(2021, 1, 13, 10, 33, 35, 962, DateTimeKind.Local).AddTicks(5151));
+
+            migrationBuilder.CreateTable(
+                name: "AppConfigs",
+                columns: table => new
+                {
+                    Key = table.Column<string>(nullable: false),
+                    Value = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppConfigs", x => x.Key);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppConfigs");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(2021, 1, 13, 10, 33, 35, 962, DateTimeKind.Local).AddTicks(5151),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2021, 1, 13, 10, 34, 17, 963, DateTimeKind.Local).AddTicks(9219));
+        }
+    }
+}
