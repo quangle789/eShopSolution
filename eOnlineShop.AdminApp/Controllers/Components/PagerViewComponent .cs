c79012c219b61f9507e4396abd34cf776471f298
quangle789 <quanglm72@wru.vn>
2021-01-22 14:41:44 +0700

    phân trang và chi tiết người dùng

---
+++ eOnlineShop.AdminApp/Controllers/Components/PagerViewComponent .cs
@@ -0,0 +1,17 @@
+﻿using eShopSolution.ViewModels.Common;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eOnlineShop.AdminApp.Controllers.Components
+{
+    public class PagerViewComponent: ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync(PagedResultPage result)
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", result));
+        }
+    }
+}
--- eShopSolution.ViewModels/Common/PageResult.cs
+++ eShopSolution.ViewModels/Common/PageResult.cs
@@ -4,9 +4,8 @@ using System.Text;
 
 namespace eShopSolution.ViewModels.Common
 {
-    public class PageResult<T>
+    public class PageResult<T> : PagedResultPage
     {
         public List<T> items { get; set; }
-        public int TotalRecord { get; set; }
     }
 }
